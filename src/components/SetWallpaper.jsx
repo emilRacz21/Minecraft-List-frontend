894dd5b1f3fe00b8997e0137c1265e937d003861
Emil Raczkiewicz <emil.raczkiewicz@gmail.com>
2025-04-18 19:15:01 +0200

    commit

--- src/components/SetWallpaper.jsx
+++ src/components/SetWallpaper.jsx
@@ -3,6 +3,7 @@ export default function SetWallpaper() {
   const hours = currentDate.getHours();
 
   document.body.classList.remove("morning", "night");
-  if (hours >= 6 && hours <= 18) document.body.classList.add("morning");
-  if (hours >= 18 && hours <= 6) document.body.classList.add("night");
+  document.body.classList.add(
+    `${hours > 6 && hours <= 18 ? "morning" : "night"}`
+  );
 }
